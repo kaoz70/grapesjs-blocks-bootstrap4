671cf2b1e6990090fbb62b2bb6028e3387867cb0
Neo Khuat <hoainam12k@gmail.com>
2018-01-04 15:32:41 +0700

    update media_object content

--- src/blocks.js
+++ src/blocks.js
@@ -60,13 +60,13 @@ export default (editor, config = {}) => {
         label: c.labels.media_object,
         category: 'Layout',
         attributes: {class:'fa fa-columns'},
-        content: '<div class="media">' +
-                 '<img class="mr-3" src="">' +
-                 '<div class="media-body">' +
-                 '<h5>Media heading</h5>' +
-                 '<div>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</div>' +
-                 '</div>' +
-                 '</div>'
+        content: `<div class="media">
+                 <img class="mr-3" src="">
+                 <div class="media-body">
+                 <h5>Media heading</h5>
+                 <div>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</div>
+                 </div>
+                 </div>`
       });
     }
 
