32113884a22b9f1a9d97816c2ad731a732b979d1
Morteza Moradi <motazad8556@gmail.com>
2019-12-10 16:22:56 +0330

    Update block.js

--- src/blocks.js
+++ src/blocks.js
@@ -301,17 +301,19 @@ export default (editor, config = {}) => {
 
   // FORMS
 
-  if (blocks.form) {
-    bm.add('form', {
-      label: `
+  if(cats.forms) {
+    
+    if (blocks.form) {
+      bm.add('form', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,5.5 C22,5.2 21.5,5 20.75,5 L3.25,5 C2.5,5 2,5.2 2,5.5 L2,8.5 C2,8.8 2.5,9 3.25,9 L20.75,9 C21.5,9 22,8.8 22,8.5 L22,5.5 Z M21,8 L3,8 L3,6 L21,6 L21,8 Z" fill-rule="nonzero"></path>
         <path class="gjs-block-svg-path" d="M22,10.5 C22,10.2 21.5,10 20.75,10 L3.25,10 C2.5,10 2,10.2 2,10.5 L2,13.5 C2,13.8 2.5,14 3.25,14 L20.75,14 C21.5,14 22,13.8 22,13.5 L22,10.5 Z M21,13 L3,13 L3,11 L21,11 L21,13 Z" fill-rule="nonzero"></path>
         <rect class="gjs-block-svg-path" x="2" y="15" width="10" height="3" rx="0.5"></rect>
       </svg>
       <div class="gjs-block-label">${c.labels.form}</div>`,
-      category: 'Forms',
-      content: `
+        category: 'Forms',
+        content: `
         <form>
           <div class="form-group">
             <label>Name</label>
@@ -338,61 +340,61 @@ export default (editor, config = {}) => {
           </div>
         </form>
       `,
-    });
-  }
+      });
+    }
 
-  if (blocks.input) {
-    bm.add('input', {
-      label: `
+    if (blocks.input) {
+      bm.add('input', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z"></path>
         <polygon class="gjs-block-svg-path" points="4 10 5 10 5 14 4 14"></polygon>
       </svg>
       <div class="gjs-block-label">${c.labels.input}</div>`,
-      category: 'Forms',
-      content: '<input class="form-control"/>',
-    });
-    bm.add('file-input', {
-      label: `
+        category: 'Forms',
+        content: '<input class="form-control"/>',
+      });
+      bm.add('file-input', {
+        label: `
             <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z"></path>
               <polygon class="gjs-block-svg-path" points="4 10 5 10 5 14 4 14"></polygon>
             </svg>
             <div class="gjs-block-label">${c.labels.file_input}</div>
         `,
-      category: 'Forms',
-      content: `<input type="file" class="form-control-file" id="exampleFormControlFile1">`
-    });
-  }
+        category: 'Forms',
+        content: `<input type="file" class="form-control-file" id="exampleFormControlFile1">`
+      });
+    }
 
-  if (blocks.form_group_input) {
-    bm.add('form_group_input', {
-      label: `
+    if (blocks.form_group_input) {
+      bm.add('form_group_input', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z"></path>
         <polygon class="gjs-block-svg-path" points="4 10 5 10 5 14 4 14"></polygon>
       </svg>
       <div class="gjs-block-label">${c.labels.form_group_input}</div>`,
-      category: 'Forms',
-      content: `
+        category: 'Forms',
+        content: `
       <div class="form-group">
         <label>Name</label>
         <input placeholder="Type here your name" class="form-control"/>
       </div>
       `,
-    });
-  }
+      });
+    }
 
-  if (blocks.input_group) {
-    bm.add('input_group', {
-      label: `
+    if (blocks.input_group) {
+      bm.add('input_group', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z"></path>
         <polygon class="gjs-block-svg-path" points="4 10 5 10 5 14 4 14"></polygon>
       </svg>
       <div class="gjs-block-label">${c.labels.input_group}</div>`,
-      category: 'Forms',
-      content: `
+        category: 'Forms',
+        content: `
       <div class="input-group">
         <div class="input-group-prepend">
           <span class="input-group-text">$</span>
@@ -403,12 +405,12 @@ export default (editor, config = {}) => {
         </div>
       </div>
       `,
-    });
-  }
+      });
+    }
 
-  if (blocks.textarea) {
-    bm.add('textarea', {
-      label: `
+    if (blocks.textarea) {
+      bm.add('textarea', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,7.5 C22,6.6 21.5,6 20.75,6 L3.25,6 C2.5,6 2,6.6 2,7.5 L2,16.5 C2,17.4 2.5,18 3.25,18 L20.75,18 C21.5,18 22,17.4 22,16.5 L22,7.5 Z M21,17 L3,17 L3,7 L21,7 L21,17 Z"></path>
         <polygon class="gjs-block-svg-path" points="4 8 5 8 5 12 4 12"></polygon>
@@ -417,83 +419,83 @@ export default (editor, config = {}) => {
         <polygon class="gjs-block-svg-path" points="20 15 21 15 21 16 20 16"></polygon>
       </svg>
       <div class="gjs-block-label">${c.labels.textarea}</div>`,
-      category: 'Forms',
-      content: '<textarea class="form-control"></textarea>',
-    });
-  }
+        category: 'Forms',
+        content: '<textarea class="form-control"></textarea>',
+      });
+    }
 
-  if (blocks.select) {
-    bm.add('select', {
-      label: `
+    if (blocks.select) {
+      bm.add('select', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z" fill-rule="nonzero"></path>
         <polygon class="gjs-block-svg-path" transform="translate(18.500000, 12.000000) scale(1, -1) translate(-18.500000, -12.000000) " points="18.5 11 20 13 17 13"></polygon>
         <rect class="gjs-block-svg-path" x="4" y="11.5" width="11" height="1"></rect>
       </svg>
       <div class="gjs-block-label">${c.labels.select}</div>`,
-      category: 'Forms',
-      content: `<select class="form-control">
+        category: 'Forms',
+        content: `<select class="form-control">
         ${c.labels.select_option ? `<option value="">${c.labels.select_option}</option>` : ''}
         <option value="1">${c.labels.option} 1</option>
         </select>`,
-    });
-  }
+      });
+    }
 
-  if (blocks.button) {
-    bm.add('button', {
-      label: `
+    if (blocks.button) {
+      bm.add('button', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,9 C22,8.4 21.5,8 20.75,8 L3.25,8 C2.5,8 2,8.4 2,9 L2,15 C2,15.6 2.5,16 3.25,16 L20.75,16 C21.5,16 22,15.6 22,15 L22,9 Z M21,15 L3,15 L3,9 L21,9 L21,15 Z" fill-rule="nonzero"></path>
         <rect class="gjs-block-svg-path" x="4" y="11.5" width="16" height="1"></rect>
       </svg>
       <div class="gjs-block-label">${c.labels.button}</div>`,
-      category: 'Forms',
-      content: '<button class="btn btn-primary">Send</button>',
-    });
-  }
+        category: 'Forms',
+        content: '<button class="btn btn-primary">Send</button>',
+      });
+    }
 
-  if (blocks.button_group) {
-    bm.add('button_group', {
-      label: c.labels.button_group,
-      category: 'Forms',
-      attributes: {class:'fa fa-link'},
-      content: {
-        type: 'button_group'
-      }
-    });
-  }
+    if (blocks.button_group) {
+      bm.add('button_group', {
+        label: c.labels.button_group,
+        category: 'Forms',
+        attributes: {class:'fa fa-link'},
+        content: {
+          type: 'button_group'
+        }
+      });
+    }
 
-  if (blocks.button_toolbar) {
-    bm.add('button_toolbar', {
-      label: c.labels.button_toolbar,
-      category: 'Forms',
-      attributes: {class:'fa fa-link'},
-      content: {
-        type: 'button_toolbar'
-      }
-    });
-  }
+    if (blocks.button_toolbar) {
+      bm.add('button_toolbar', {
+        label: c.labels.button_toolbar,
+        category: 'Forms',
+        attributes: {class:'fa fa-link'},
+        content: {
+          type: 'button_toolbar'
+        }
+      });
+    }
 
-  if (blocks.label) {
-    bm.add('label', {
-      label: `
+    if (blocks.label) {
+      bm.add('label', {
+        label: `
       <svg class="gjs-block-svg" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path class="gjs-block-svg-path" d="M22,11.875 C22,11.35 21.5,11 20.75,11 L3.25,11 C2.5,11 2,11.35 2,11.875 L2,17.125 C2,17.65 2.5,18 3.25,18 L20.75,18 C21.5,18 22,17.65 22,17.125 L22,11.875 Z M21,17 L3,17 L3,12 L21,12 L21,17 Z" fill-rule="nonzero"></path>
         <rect class="gjs-block-svg-path" x="2" y="5" width="14" height="5" rx="0.5"></rect>
         <polygon class="gjs-block-svg-path" fill-rule="nonzero" points="4 13 5 13 5 16 4 16"></polygon>
       </svg>
       <div class="gjs-block-label">${c.labels.label}</div>`,
-      category: 'Forms',
-      content: '<label>Label</label>',
-    });
-  }
+        category: 'Forms',
+        content: '<label>Label</label>',
+      });
+    }
 
-  if (blocks.checkbox) {
-    bm.add('checkbox', {
-      label: c.labels.checkbox,
-      attributes: {class:'fa fa-check-square'},
-      category: 'Forms',
-      content: `
+    if (blocks.checkbox) {
+      bm.add('checkbox', {
+        label: c.labels.checkbox,
+        attributes: {class:'fa fa-check-square'},
+        category: 'Forms',
+        content: `
         <div class="form-check">
           <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
           <label class="form-check-label" for="defaultCheck1">
@@ -501,15 +503,15 @@ export default (editor, config = {}) => {
           </label>
         </div>
       `,
-    });
-  }
+      });
+    }
 
-  if (blocks.radio) {
-    bm.add('radio', {
-      label: c.labels.radio,
-      attributes: {class:'fa fa-dot-circle-o'},
-      category: 'Forms',
-      content: `
+    if (blocks.radio) {
+      bm.add('radio', {
+        label: c.labels.radio,
+        attributes: {class:'fa fa-dot-circle-o'},
+        category: 'Forms',
+        content: `
         <div class="form-check">
           <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
           <label class="form-check-label" for="exampleRadios1">
@@ -517,7 +519,9 @@ export default (editor, config = {}) => {
           </label>
         </div>
       `,
-    });
+      });
+    }
+    
   }
 
 }
